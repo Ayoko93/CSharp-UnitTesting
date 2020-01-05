8288677a13f8633664fb8bcff1950d7319bcc602
Adrian <Adrian.Juettner@live.de>
2020-01-05 19:05:34 +0100

    Added some tests for Stack and DemeritPointsCalculator

---
+++ TestNinja/TestNinja.UnitTest/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    class DemeritPointsCalculatorTests
+    {
+
+        DemeritPointsCalculator calculator;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            calculator = new DemeritPointsCalculator();
+        }
+
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(50, 0)]
+        [TestCase(65, 0)]
+        [TestCase(69, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedDemeritPoints)
+        {
+            int result = calculator.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(expectedDemeritPoints));
+        }
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+
+            Assert.That(() => calculator.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+
+        }
+
+
+
+    }
+}
---
+++ TestNinja/TestNinja.UnitTest/StackTest.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    class StackTest
+    {
+        private Stack<string> stack;
+
+        [SetUp]
+        public void SetUp()
+        {
+            stack = new Stack<string>();
+        }
+
+
+        [Test]
+        public void Push_NullAsArgument_ThrowArgumentNullException()
+        {
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Push_StackIsEmpty_CountIsOne()
+        {
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_StackIsEmpty_ThrowInvalidOperationException()
+        {
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+
+        [Test]
+        public void Pop_StackHasOneElement_ReturnElementOnTheTop()
+        {
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            var result = stack.Pop();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackHasElements_RemoveElementOnTheTop()
+        {
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            stack.Pop();
+
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_StackIsEmpty_ThrowInvalidOperationException()
+        {
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackHasElements_ReturnElementOnTheTop()
+        {
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            var result = stack.Peek();
+
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackHasOneElement_RemoveElementOnTheTop()
+        {
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            stack.Peek();
+
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
+
+
+    }
+}
