9767af67f0cc13a7893ea8b3a7628eda29111f70
Adrian <Adrian.Juettner@live.de>
2020-01-05 17:52:19 +0100

    Added some tests

---
+++ TestNinja/TestNinja.UnitTest/FizzBuzzTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    class FizzBuzzTest
+    {
+        [Test]
+        public void GetOutput_NumberIsDividableByThreeAndFive_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz").IgnoreCase);
+        }
+        
+        [Test]
+        public void GetOutput_NumberIsDividableByThree_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz").IgnoreCase);
+        }
+        
+        [Test]
+        public void GetOutput_NumberIsDividableByFive_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz").IgnoreCase);
+        }
+        [Test]
+        public void GetOutput_NumberIsNotDividableByThreeOrFive_ReturnNumber()
+        {
+            var result = FizzBuzz.GetOutput(2);
+
+            Assert.That(result, Is.EqualTo("2").IgnoreCase);
+        }
+    }
+}
---
+++ TestNinja/TestNinja.UnitTest/MathTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    class MathTest
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this._math = new Math();
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbers()
+        {
+            var result = _math.GetOddNumbers(5);
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsZero_ReturnEmptyArray()
+        {
+            var result = _math.GetOddNumbers(0);
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsNegative_ReturnEmptyArray()
+        {
+            var result = _math.GetOddNumbers(-5);
+            Assert.That(result, Is.Empty);
+        }
+
+
+    }
+}
